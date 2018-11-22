50facee6be0ce6de17f2cdbb420af947dac81fdb
Joe <wujun-00@qq.com>
2018-11-22 19:35:24 +0800

    queue

---
+++ queueStudy.py
@@ -0,0 +1,32 @@
+# import queue
+
+class Queue:
+  def __init__(self):
+    self.items = []
+
+  def isEmpty(self):
+    return self.items == []
+
+  def enqueue(self, item):
+    self.items.insert(0,item)
+
+  def dequeue(self):
+    return self.items.pop()
+
+  def size(self):
+    return len(self.items)
+
+def hotPotato(nameList,num):
+  simqueue = Queue()
+
+  for name in nameList:
+    simqueue.enqueue(name)
+
+  while simqueue.size()>1:
+    for i in range(num):
+      simqueue.enqueue(simqueue.dequeue())
+    simqueue.dequeue()
+
+  return simqueue.dequeue()
+
+print(hotPotato(['1','2','3','4','5','6'],7))
